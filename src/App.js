ce07daa28b6dd39aded16ba248a500d0c86b32c9
cent kanayo <centkanayo@gmail.com>
2022-11-30 11:48:32 +0000

    new class 30th nov 2022

--- src/App.js
+++ src/App.js
@@ -1,91 +1,100 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const App = () => {
-  // const [userInfo, setUserInfo] = useState({
-  //   username: '',
-  //   password: '',
-  //   email: '',
-  // });
+  const [player1Score, setPlayer1Score] = useState(0);
+  const [player2Score, setPlayer2core] = useState(0);
+  const [player1, setPlayer1] = useState(0);
+  const [player2, setPlayer2] = useState(0);
+  const [round, setRound] = useState(1);
 
-  const username = useRef(null);
-  const email = useRef(null);
-  const password = useRef(null);
-
-  const [users, setUsers] = useState([]);
-  // const handleChange = (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setUserInfo((prev) => {
-  //     return {
-  //       ...prev,
-  //       [name]: value,
-  //     };
-  //   });
-  // };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const name = username.current.value;
-    const userEmail = email.current.value;
-    const userPassword = password.current.value;
-
-    const user = { username: name, email: userEmail, password: userPassword };
-    setUsers((prev) => [...prev, user]);
+  const handlePlay = () => {
+    const player1 = Math.floor(Math.random() * 10) + 1;
+    const player2 = Math.floor(Math.random() * 10) + 1;
+    setPlayer1(player1);
+    setPlayer2(player2);
+    setRound(round + 1);
   };
+  useEffect(() => {
+    const sum = player1 + player2;
+    if (sum % 2 === 0) {
+      setPlayer1Score(player1Score + 1);
+    } else {
+      setPlayer2core(player2Score + 1);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Register new User</h1>
-      <form
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="">Username: </label>
-        <input type="text" required ref={username} />
-        <label htmlFor="">Email: </label>
-        <input type="text" required ref={email} />
-        <label htmlFor="">Password: </label>
-        <input type="text" ref={password} required />
-        <button>Submit</button>
-      </form>
-      <div>
-        {users.map((user, index) => {
-          return (
-            <article key={index}>
-              <h1>{user.username}</h1>
-              <h3>{user.email}</h3>
-              <h3>{user.password}</h3>
-            </article>
-          );
-        })}
-      </div>
+      <h1>Round: {round}</h1>
+      <h1>Player1 score: {player1Score} </h1>
+      <h1>Player2 score: {player2Score} </h1>
+
+      <h3>Player1 guess is: {player1}</h3>
+      <h3>Player2 guess is: {player2}</h3>
+      <button onClick={handlePlay}>Play </button>
     </div>
   );
 };
+
 export default App;
 
-// const [count, setCount] = useState(0);
-// const [userInput, setUserInput] = useState(null);
+//  useEffect(() => {
+//    const sum = player1 + player2;
+//    if (sum % 2 === 0) {
+//      setPlayer1Score(player1Score + 1);
+//    } else {
+//      setPlayer2core(player2Score + 1);
+//    }
+//  }, [player1, player2]);
+{
+  /* <h1>Round: {round}</h1>
+      <h1>Player1 score: {player1Score} </h1>
+      <h1>Player2 score: {player2Score} </h1>
+
+      <h3>Player1 guess is: {player1}</h3>
+      <h3>Player2 guess is: {player2}</h3>
+      <button onClick={handlePlay}>Play </button> */
+}
+
+// const [user, setUser] = useState({});
+// const [username, setUsername] = useState('cent-kanayo');
+// const [loading, setLoading] = useState(false);
+// const userRef = useRef(null);
+// const url = `https://api.github.com/users/${username}`;
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       setLoading(true);
+//       const response = await fetch(url);
+//       const data = await response.json();
+//       setUser(data);
+//       setLoading(false);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   fetchData();
+// }, [username]);
 // const handleSubmit = (e) => {
 //   e.preventDefault();
-//   const value = +userInput || 0;
-//   setCount(count + value);
+//   if (!userRef.current.value) {
+//     return;
+//   }
+//   setUsername(userRef.current.value);
 // };
+// if (loading) return <h1>Loading...</h1>;
 // return (
-//   <div style={{ display: 'grid', gap: '10px', placeItems: 'center' }}>
-//     <h2>Welcome to my count app</h2>
-//     <h1>{count}</h1>
-//     <div style={{ fontSize: '36px', display: 'flex', gap: '10px' }}>
-//       <button onClick={() => setCount(count + 1)}>Increase</button>
-//       <button onDoubleClick={() => setCount(0)}>Reset</button>
-//       <button onClick={() => setCount(count - 1)}>Decrease</button>
-//     </div>
+//   <div>
 //     <form onSubmit={handleSubmit}>
-//       <label>Enter a number</label>
 //       <input
 //         type="text"
-//         value={userInput}
-//         onChange={(e) => setUserInput(e.target.value)}
+//         ref={userRef}
+//         placeholder="enter your github username"
 //       />
-//       <button>Submit</button>
+//       <button>submit</button>
 //     </form>
+//     <h1>Github user profile</h1>
+//     <h1>{user.name}</h1>
+//     <img src={user.avatar_url} alt="" />
 //   </div>
 // );
